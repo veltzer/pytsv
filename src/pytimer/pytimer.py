220c012855a5a230aed5b673736c3a3e8ce75dc2
Mark Veltzer <mark.veltzer@gmail.com>
2016-12-21 14:55:13 +0200

    better printing

--- src/pytimer/pytimer.py
+++ src/pytimer/pytimer.py
@@ -2,11 +2,16 @@ import time
 
 
 class Timer():
-    def __init__(self, print=True):
+    def __init__(self, print=True, title=None):
         self.start_time = None
+        self.title = title
     def __enter__(self):
         self.start_time = time.time()
     def __exit__(self, itype, value, traceback):
         self.end_time = time.time()
         diff = self.end_time - self.start_time
-        print('time taken: {0:.6f} seconds'.format(diff))
+        if self.print:
+            if title:
+                print('time taken for {title}: {0:.6fdiff} seconds'.format(title=self.title, diff=diff))
+            else:
+                print('time taken: {0:.6fdiff} seconds'.format(diff=diff))
