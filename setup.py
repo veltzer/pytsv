e054ab9774da9a81b6290f2f8a75fdad0308a1e7
Mark Veltzer <mark.veltzer@gmail.com>
2016-12-21 11:17:40 +0200

    first version

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name='pytimer',
+    version='0.0.1',
+    description='pytimer is an easy to use timer',
+    long_description='context based timer to easily time your code',
+    url='https://veltzer.github.io/pytimer',
+    author='Mark Veltzer',
+    author_email='mark.veltzer@gmail.com',
+    license='GPL3',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='python timing time context manager utility',
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages('src'),
+)
---
+++ src/pytimer/pytimer.py
@@ -0,0 +1,12 @@
+import time
+
+
+class Timer():
+    def __init__(self, print=True):
+        self.start_time = None
+    def __enter__(self):
+        self.start_time = time.time()
+    def __end__(self):
+        self.end_time = time.time()
+        diff = self.end_time - self.start_time
+        print('time taken: {0:.6f} seconds'.format(diff))
