3df69f632da61ee4e13a096fc68d36f77ebb34c4
Mark Veltzer <mark.veltzer@gmail.com>
2017-03-08 17:54:08 +0200

    tsvcheck can now audo detect field number

--- src/pytsv/check.py
+++ src/pytsv/check.py
@@ -3,7 +3,7 @@ import sys
 
 
 @click.command()
-@click.option('--num-fields', required=True, type=int, help="how many fields should the tsv have")
+@click.option('--num-fields', required=False, default=None, type=int, help="how many fields should the tsv have")
 @click.argument('input-files', nargs=-1)
 def main(num_fields, input_files):
     """ This script checks that every file given to it is legal tsv """
@@ -12,6 +12,8 @@ def main(num_fields, input_files):
             for line in input_file_handle:
                 line = line.rstrip('\r\n')
                 parts = line.split("\t")
+                if num_fields is None:
+                    num_fields = len(parts)
                 if len(parts) != num_fields:
                     print("line [{}] in file [{}] has errors has [{}] parts instead of [{}]".format(
                         line, input_file, len(parts), num_fields))
