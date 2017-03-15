a261fc9003f70a98a56495629312b38a807afb05
Mark Veltzer <mark.veltzer@gmail.com>
2017-03-15 15:18:56 +0200

    new release

--- src/pytsv/check.py
+++ src/pytsv/check.py
@@ -7,7 +7,7 @@ from pytsv.pytsv import TsvReader
 @click.command()
 @click.option('--num-fields', required=False, default=None, type=int, help="how many fields should the tsv have")
 @click.option('--progress', required=False, default=True, type=bool, help="show progress")
-@click.argument('input-files', nargs=-1, help="the files to check")
+@click.argument('input-files', nargs=-1)
 def main(num_fields, progress, input_files):
     """ This script checks that every file given to it is legal tsv """
     for input_file in input_files:
