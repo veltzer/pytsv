f81466f487a17e7fe45377d1106403a272772169
Mark Veltzer <mark.veltzer@gmail.com>
2017-03-27 14:30:19 +0300

    fixed number of fields bug

--- src/pytsv/scripts/check.py
+++ src/pytsv/scripts/check.py
@@ -10,17 +10,18 @@ TODO:
 
 
 @click.command()
-@click.option('--num-fields', required=False, default=0, type=int, help="how many fields should the tsv have")
+@click.option('--num-fields', required=False, default=None, type=int, help="how many fields should the tsv have")
 @click.option('--progress', required=False, default=True, type=bool, help="show progress")
+@click.option('--check-non-ascii', required=False, default=True, type=bool, help="check non ascii")
 @click.option('--validate_all_lines_same_number_of_fields', required=False, default=True, type=bool,
               help="validate all lines same number of fields")
 @click.argument('input-files', nargs=-1)
-def main(num_fields, progress, validate_all_lines_same_number_of_fields, input_files):
+def main(num_fields, progress, check_non_ascii, validate_all_lines_same_number_of_fields, input_files):
     """ This script checks that every file given to it is legal tsv """
     for input_file in input_files:
         with TsvReader(filename=input_file, num_fields=num_fields,
                        validate_all_lines_same_number_of_fields=validate_all_lines_same_number_of_fields,
-                       check_non_ascii=True) as input_file_handle:
+                       check_non_ascii=check_non_ascii) as input_file_handle:
             if progress:
                 input_file_handle = tqdm.tqdm(input_file_handle)
             for _ in input_file_handle:
