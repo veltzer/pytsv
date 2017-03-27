2778a6cdca0a56db85f6bfd00afb401c3f868102
Mark Veltzer <mark.veltzer@gmail.com>
2017-03-27 14:34:05 +0300

    added filename visibility

--- src/pytsv/scripts/check.py
+++ src/pytsv/scripts/check.py
@@ -12,13 +12,16 @@ TODO:
 @click.command()
 @click.option('--num-fields', required=False, default=None, type=int, help="how many fields should the tsv have")
 @click.option('--progress', required=False, default=True, type=bool, help="show progress")
+@click.option('--filename', required=False, default=True, type=bool, help="show filename")
 @click.option('--check-non-ascii', required=False, default=True, type=bool, help="check non ascii")
 @click.option('--validate_all_lines_same_number_of_fields', required=False, default=True, type=bool,
               help="validate all lines same number of fields")
 @click.argument('input-files', nargs=-1)
-def main(num_fields, progress, check_non_ascii, validate_all_lines_same_number_of_fields, input_files):
+def main(num_fields, progress, filename, check_non_ascii, validate_all_lines_same_number_of_fields, input_files):
     """ This script checks that every file given to it is legal tsv """
     for input_file in input_files:
+        if filename:
+            print('checking [{}]...'.format(filename))
         with TsvReader(filename=input_file, num_fields=num_fields,
                        validate_all_lines_same_number_of_fields=validate_all_lines_same_number_of_fields,
                        check_non_ascii=check_non_ascii) as input_file_handle:
