9a8738c15aa5391c4714520b62b07a101e5fe21f
Mark Veltzer <mark.veltzer@gmail.com>
2017-04-08 16:25:52 +0300

    new python version

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 import setuptools
 
-import sys
-if not sys.version_info[0] == 3:
-    sys.exit("Sorry, only python version 3 is supported")
+#import sys
+#if not sys.version_info[0] == 3:
+#    sys.exit("Sorry, only python version 3 is supported")
 
 setuptools.setup(
     name='pytsv',
