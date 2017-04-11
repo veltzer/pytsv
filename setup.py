9b3861dc013a71c78ca3fe6710b55229badb9180
Mark Veltzer <mark.veltzer@gmail.com>
2017-04-11 14:32:00 +0300

    bugtrack_url did not work

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ if not sys.version_info[0] == 3:
 
 setuptools.setup(
     name='pytsv',
-    version='0.0.49',
+    version='0.0.50',
     description='pytsv is a module to help with all things TSV',
     long_description='pytsv is a module to help with all things TSV',
     url='https://veltzer.github.io/pytsv',
-    bugtrack_url='https://github.com/veltzer/pytsv/issues',
+    # bugtrack_url='https://github.com/veltzer/pytsv/issues',
     author='Mark Veltzer',
     author_email='mark.veltzer@gmail.com',
     maintainer='Mark Veltzer',
