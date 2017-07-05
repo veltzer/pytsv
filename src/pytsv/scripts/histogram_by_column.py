b3322762ddd6e56288b23abb4d6e8faeb806049b
Mark Veltzer <mark.veltzer@gmail.com>
2017-07-05 19:28:26 +0300

    added percentiles

--- src/pytsv/scripts/histogram_by_column.py
+++ src/pytsv/scripts/histogram_by_column.py
@@ -17,18 +17,23 @@ def main(
 ) -> None:
     """ Create a histogram from a field in a tsv file """
     a = []
+    total = 0
     with TsvReader(input_file) as input_handle:
         for fields in input_handle:
             a.append(float(fields[column_number]))
+            total += 1
     count_in_bucket, bucket_edges = numpy.histogram(a, bins=bucket_number)
     with TsvWriter(output_file) as output_handle:
+        current_sum = 0
         for i, count in enumerate(count_in_bucket):
+            current_sum += count
             edge_from = bucket_edges[i]
             edge_to = bucket_edges[i+1]
             output_handle.write([
                 str(edge_from),
                 str(edge_to),
                 str(count),
+                str(int(100.0*current_sum/total)),
             ])
 
 if __name__ == '__main__':
