1516b88a65b25a939ab4b4b41f3e6326ba1df257
Mark Veltzer <mark.veltzer@gmail.com>
2017-10-25 19:29:26 +0300

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='pytsv',
-    version='0.1.4',
+    version='0.1.5',
     description='pytsv is a module to help with all things TSV',
     long_description='pytsv is a module to help with all things TSV',
     url='https://github.com/veltzer/pytsv',
