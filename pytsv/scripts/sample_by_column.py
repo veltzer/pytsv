8d4032b4c5170f8e3a436e5446b4fdd19ffd1b11
Mark Veltzer <mark.veltzer@gmail.com>
2017-10-25 16:20:30 +0300

    added sample by column

--- pytsv/scripts/sample_by_column.py
+++ pytsv/scripts/sample_by_column.py
@@ -5,20 +5,6 @@ import numpy as np
 
 @click.command()
 @click.option(
-    '--sample-column',
-    required=True,
-    type=str,
-    help="what columns to split by, comma separated",
-    show_default=True,
-)
-@click.option(
-    '--size',
-    required=True,
-    type=int,
-    help="what sample size do you need?",
-    show_default=True,
-)
-@click.option(
     '--input-file',
     required=True,
     type=str,
@@ -32,20 +18,32 @@ import numpy as np
     help="output filename",
     show_default=True,
 )
+@click.option(
+    '--sample-column',
+    required=True,
+    type=str,
+    help="what column to sample by",
+    show_default=True,
+)
+@click.option(
+    '--size',
+    required=True,
+    type=int,
+    help="what sample size do you need?",
+    show_default=True,
+)
 def main(
-        factor_column,
-        sample_columns,
-        size,
         input_file,
         output_file,
+        sample_column,
+        size,
 ):
-    # type: (int, str, int, str, str) -> None
+    # type: (str, str, int, int) -> None
     """
-    This application will sample a set of a tsv file.
+    This application will sample from a tsv file by a weight column
     """
-    sample_columns = [int(x) for x in sample_columns.split(',')]
     df = pd.read_csv(input_file, index_col=False, header=None, sep="\t")
-    df.sample(n=size, weights=np.log2(df[factor_column]) + 1)[sample_columns].to_csv(
+    df.sample(n=size, weights=np.log2(df[sample_column]) + 1).to_csv(
         output_file,
         sep='\t',
         index=False,
