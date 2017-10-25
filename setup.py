5404975c0d98f05e4a25e727c6c6c0462861b554
Mark Veltzer <mark.veltzer@gmail.com>
2017-10-25 17:54:01 +0300

    added new line support

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='pytsv',
-    version='0.0.99',
+    version='0.1.00',
     description='pytsv is a module to help with all things TSV',
     long_description='pytsv is a module to help with all things TSV',
     url='https://github.com/veltzer/pytsv',
