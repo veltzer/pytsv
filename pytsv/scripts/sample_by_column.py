0e151263fc5763f55a7bb6d331f1b97d5c4c200c
Mark Veltzer <mark.veltzer@gmail.com>
2017-10-26 15:56:50 +0300

    better version

--- pytsv/scripts/sample_by_column.py
+++ pytsv/scripts/sample_by_column.py
@@ -11,28 +11,30 @@ import numpy.random
     required=True,
     type=str,
     help="input filename",
-    show_default=True,
 )
 @click.option(
     '--output-file',
     required=True,
     type=str,
     help="output filename",
-    show_default=True,
 )
 @click.option(
     '--sample-column',
     required=True,
     type=int,
     help="what column to sample by",
-    show_default=True,
 )
 @click.option(
     '--size',
     required=True,
     type=int,
     help="what sample size do you need?",
-    show_default=True,
+)
+@click.option(
+    '--replace',
+    required=True,
+    type=bool,
+    help="allow replacement",
 )
 @click.option(
     '--progress',
@@ -47,6 +49,7 @@ def main(
         output_file,
         sample_column,
         size,
+        replace,
         progress,
 ):
     # type: (str, str, int, int, bool) -> None
@@ -56,12 +59,14 @@ def main(
     """
     weights = []
     elements = []
+    sum_weights = float(0)
     with TsvReader(input_file) as input_handle:
         if progress:
             input_handle = tqdm.tqdm(input_handle)
         for fields in input_handle:
             elements.append(fields)
             weight = float(fields[sample_column])
+            sum_weights += weight
             weights.append(weight)
     # the following code will only work on python3.6 because the
     # random.choices API was only introduced then
@@ -69,10 +74,16 @@ def main(
     # results = choices(lines, weights, k=size)
 
     # this is the same code with numpy
-    results = [numpy.random.choice(elements, p=weights) for _ in range(size)]
+    weights = [w/sum_weights for w in weights]
+    results = numpy.random.choice(
+        a=len(elements),
+        replace=replace,
+        size=size,
+        p=weights,
+    )
     with TsvWriter(output_file) as output_handle:
         for result in results:
-            output_handle.write(result)
+            output_handle.write(elements[result])
 
 
 if __name__ == '__main__':
