ba536408e10936ba0c8c442fcb0efcbf62d4a52d
Mark Veltzer <mark.veltzer@gmail.com>
2017-11-15 13:26:01 +0200

    version now available to python code

--- pytsv/scripts/test.py
+++ pytsv/scripts/test.py
@@ -2,14 +2,28 @@ import click
 
 import numpy.random
 
+import pytsv.version
+
 
 @click.command()
+@click.option(
+    '--version',
+    required=False,
+    default=False,
+    type=bool,
+    help="show version",
+    show_default=True,
+)
 def main(
+    version,
 ):
-    # type: () -> None
+    # type: (bool) -> None
     """
     Test application
     """
+    if version:
+        print(pytsv.version.version_str)
+        return
     print(numpy.version.full_version)
 
 
