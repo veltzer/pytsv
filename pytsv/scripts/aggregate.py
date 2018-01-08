ce45e19f30102434f466443e92413341ce98cd36
Mark Veltzer <mark.veltzer@gmail.com>
2018-01-08 13:40:23 +0200

    aggregate is now better

--- pytsv/scripts/aggregate.py
+++ pytsv/scripts/aggregate.py
@@ -1,16 +1,10 @@
-import os
 import click
+from typing import List
+
 import pytsv.pytsv
 
 
 @click.command()
-@click.option(
-    '--input-folder',
-    required=True,
-    type=str,
-    help="folder of input files",
-    show_default=True,
-)
 @click.option(
     '--output-file',
     required=True,
@@ -22,41 +16,36 @@ import pytsv.pytsv
     '--match-columns',
     required=True,
     type=str,
-    help="column to match by",
+    help="column to match by (list of numbers separated by comma)",
     show_default=True,
 )
 @click.option(
     '--aggregate-columns',
     required=True,
     type=str,
-    help="column to aggregate (must be numeric)",
+    help="column to aggregate by (list of numbers separated by comma)",
     show_default=True,
 )
-@click.option(
-    '--do-unlink',
-    required=False,
-    type=bool,
-    default=False,
-    help="remove files after done?",
-    show_default=True,
+@click.argument(
+    'input-files',
+    nargs=-1,
+    required=True,
 )
 def main(
-        input_folder,
         output_file,
         match_columns,
         aggregate_columns,
-        do_unlink,
+        input_files,
 ):
-    # type: (str, str, str, str, bool) -> None
-    """ aggregate a bunch of tsv files """
+    # type: (str, str, str, List[str]) -> None
+    """ aggregate tsv files """
     match_columns = [int(x) for x in match_columns.split(",")]
     aggregate_columns = [int(x) for x in aggregate_columns.split(",")]
     pytsv.pytsv.aggregate(
-        input_file_names=os.listdir(input_folder),
+        input_file_names=input_files,
         match_columns=match_columns,
         aggregate_columns=aggregate_columns,
         output_file_name=output_file,
-        unlink=do_unlink,
     )
 
 
