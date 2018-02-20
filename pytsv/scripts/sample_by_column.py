a56c038abbf8ea18d87c835432a58824c1b8822f
Mark Veltzer <mark.veltzer@gmail.com>
2018-02-20 11:19:16 +0200

    added checking that values are unique

--- pytsv/scripts/sample_by_column.py
+++ pytsv/scripts/sample_by_column.py
@@ -1,6 +1,9 @@
+import logging
+
 import click
 
 import pandas
+import pylogconf.core
 
 
 @click.command()
@@ -41,6 +44,13 @@ import pandas
     default=False,
     help="allow replacements?",
 )
+@click.option(
+    '--check-unique',
+    required=False,
+    type=bool,
+    default=True,
+    help="check that the value_column has unique values?",
+)
 def main(
         input_file,
         output_file,
@@ -48,23 +58,36 @@ def main(
         value_column,
         size,
         replace,
+        check_unique,
 ):
-    # type: (str, str, int, int, int, bool) -> None
+    # type: (str, str, int, int, int, bool, bool) -> None
     """
-    This application will sample from a tsv file by a sample column
-    The sample column must be convertible to a floating point number.
+    This application will create a weighted sample from a tsv file.
+    To run this you must supply a 'value_column' (the column
+    which will be sampled) and a 'weight_column' which must
+    be convertible to a floating point number.
     """
+    logger = logging.getLogger(__name__)
+    logger.info("reading the data")
     df = pandas.read_csv(
         input_file,
         sep='\t',
         header=None,
     )
+    logger.info("checking that the values are unique")
+    unique_values_count = df[value_column].nunique()
+    if check_unique and unique_values_count != df.shape[0]:
+        logger.error("your data is not unique in the value_column")
+        logger.error("unique values {} != number of rows {}".format(unique_values_count, df.shape[0]))
+        return
+    logger.info("sampling")
     sample = df.sample(
         n=size,
         replace=replace,
         weights=df[weight_column],
     )
     df_result = sample[value_column].value_counts()
+    logger.info("writing the output")
     df_result.to_csv(
         output_file,
         sep='\t',
@@ -73,4 +96,5 @@ def main(
 
 
 if __name__ == '__main__':
+    pylogconf.core.setup()
     main()
