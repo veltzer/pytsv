e0cfb8f7c0505680235bd5a925e296bdf9eeef06
Mark Veltzer <mark.veltzer@gmail.com>
2019-07-26 17:42:04 +0300

    new version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ http://setuptools.readthedocs.io/en/latest/setuptools.html
 setuptools.setup(
     # the first three fields are a must according to the documentation
     name='pytsv',
-    version='0.1.59',
+    version='0.1.60',
     packages=[
         'pytsv',
         'pytsv.endpoints',
