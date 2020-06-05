701b03d76d7783e09a121a7a5f591a842cdc9b24
Mark Veltzer <mark.veltzer@gmail.com>
2020-06-06 00:52:55 +0300

    new template

--- setup.py
+++ setup.py
@@ -2,28 +2,29 @@ import setuptools
 
 setuptools.setup(
     # the first three fields are a must according to the documentation
-    name='pytsv',
-    version='0.1.63',
+    name="pytsv",
+    version="0.1.63",
     packages=[
         'pytsv',
         'pytsv.endpoints',
     ],
     # from here all is optional
-    description='pytsv is a module to help with all things TSV',
-    long_description='pytsv is a module to help with all things TSV',
-    author='Mark Veltzer',
-    author_email='mark.veltzer@gmail.com',
-    maintainer='Mark Veltzer',
-    maintainer_email='mark.veltzer@gmail.com',
+    description="pytsv is a module to help with all things TSV",
+    long_description="pytsv is a module to help with all things TSV",
+    long_description_content_type="text/x-rst",
+    author="Mark Veltzer",
+    author_email="mark.veltzer@gmail.com",
+    maintainer="Mark Veltzer",
+    maintainer_email="mark.veltzer@gmail.com",
     keywords=[
         'python',
         'tsv',
         'format',
         'csv',
     ],
-    url='https://veltzer.github.io/pytsv',
-    download_url='https://github.com/veltzer/pytsv',
-    license='MIT',
+    url="https://veltzer.github.io/pytsv",
+    download_url="https://github.com/veltzer/pytsv",
+    license="MIT",
     platforms=[
         'python3',
     ],
@@ -47,8 +48,8 @@ setuptools.setup(
     ],
     data_files=[
     ],
-    entry_points={'console_scripts': [
+    entry_points={"console_scripts": [
         'pytsv=pytsv.endpoints.main:main',
     ]},
-    python_requires='>=3.5',
+    python_requires=">=3.5",
 )
