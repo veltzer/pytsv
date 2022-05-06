973a458df49042bd7abf25d72094a23eaf43d0dd
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-06 14:12:35 +0300

    better deps

--- config/python.py
+++ config/python.py
@@ -4,42 +4,33 @@ package_name = config.project.project_name
 
 
 console_scripts = [
-    'pytsv=pytsv.main:main',
-]
-
-setup_requires = [
+    "pytsv=pytsv.main:main",
 ]
 
 run_requires = [
-    'pytconf',
-    'tqdm',
-    'pyanyzip',
-    'numpy',
-    'pandas',
-    'pylogconf',
-    'attrs',
+    "pytconf",
+    "tqdm",
+    "pyanyzip",
+    "numpy",
+    "pandas",
+    "pylogconf",
+    "attrs",
 ]
 
 test_requires = [
-    'pylint',
-    'pytest',
-    'pytest-cov',
-    'pyflakes',
-    'flake8',
-    'pymakehelper',
+    "pylint",
+    "pytest",
+    "pytest-cov",
+    "pyflakes",
+    "flake8",
+    "pymakehelper",
 ]
 
 dev_requires = [
-    'pyclassifiers',
-    'pypitools',
-    'pydmt',
+    "pyclassifiers",
+    "pypitools",
+    "pydmt",
 ]
-
-install_requires = list(setup_requires)
-install_requires.extend(run_requires)
-
-extras_require = {
-}
 
 python_requires = ">=3.9"
 test_os = ["ubuntu-20.04"]
