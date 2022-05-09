876a353345753f61d18d36e51112ed8314409c74
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-09 13:11:43 +0300

    new version of python

--- setup.py
+++ setup.py
@@ -33,6 +33,15 @@ setuptools.setup(
     platforms=[
         'python3',
     ],
+    install_requires=[
+        'pytconf',
+        'tqdm',
+        'pyanyzip',
+        'numpy',
+        'pandas',
+        'pylogconf',
+        'attrs',
+    ],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
@@ -49,5 +58,5 @@ setuptools.setup(
     entry_points={"console_scripts": [
         'pytsv=pytsv.main:main',
     ]},
-    python_requires=">=3.9",
+    python_requires=">=3.10",
 )
