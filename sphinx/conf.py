b7d0e76ba9c51e9f2f102e98901d51db8a1fa8e8
Mark Veltzer <mark.veltzer@gmail.com>
2023-04-29 00:31:55 +0300

    new pydmt

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -2,8 +2,19 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.coverage',
               'sphinx.ext.viewcode']
 
-project = 'pytsv'
-author = 'Mark Veltzer'
-project_copyright = "2016 - 2022"+', '+author
-version = '0.0.1'
-release = '0.0.1'
+import config.project
+project = config.project.name
+import config.personal
+author = config.personal.fullname
+# FIXME
+project_copyright = author
+import config.version
+version = ".".join(str(x) for x in config.version.tup)
+release = ".".join(str(x) for x in config.version.tup)
+
+html_theme_options = {
+        "show_powered_by": "false",
+}
+rst_epilog = f'''
+.. |project| replace:: {project}
+'''
